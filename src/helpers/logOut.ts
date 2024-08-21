32cec08ef3b001466d4fd132da3417b970143ae6
demos-prog <73884833+demos-prog@users.noreply.github.com>
2024-08-21 11:22:33 +0300

    test3

--- src/helpers/logOut.ts
+++ src/helpers/logOut.ts
@@ -2,5 +2,5 @@ import { CLIENT_URL } from "../constants";
 
 export default function logOut() {
   localStorage.removeItem('myAppUsersData');
-  window.location.assign(`${CLIENT_URL}/login`);
+  window.location.assign(CLIENT_URL);
 }
